caed35aebbb97db08aa0c1a82fc946c2a6dcb340
Simon-Cederbom <Simon.cederbom@gmail.com>
2018-05-09 15:03:06 +0200

    circle fixad. rätt color i chess är kvar att göra

--- app/Chess.java
+++ app/Chess.java
@@ -7,27 +7,28 @@ import java.awt.image.BufferedImage;
 import framework.Edit;
 
 /*TODO
- * Kanske inte såhär de menar med chessfilter. 
+ * Fixa färgerna till att bara bli mörkare och inte galna.
  */
 
 public class Chess extends Edit {
 
 	public BufferedImage edit(BufferedImage img) {
 
-		Graphics g = img.getGraphics();
-		g.setColor(new Color(0, 0, 0));
-		
+		boolean white = true;
+
 		for (int x = 0; x < img.getWidth(); x++) {
-			if (x % (img.getWidth() / 11) == 0 && x > img.getWidth()%11 && x < img.getWidth() - img.getWidth()%11) {
-				g.fillRect(x, 0, img.getWidth() % 11, img.getHeight());
-				x += img.getWidth() % 11;
+			if (x % (img.getWidth() / 11) == 0 && x > img.getWidth() % 11 && x < img.getWidth() - img.getWidth() % 11) {
+				white = !white;
 			}
-		}
-		
-		for (int y = 0; y < img.getHeight(); y++) {
-			if (y % (img.getHeight() / 11) == 0 && y > img.getHeight()%11 && y < img.getHeight() - img.getHeight()%11) {
-				g.fillRect(0, y, img.getWidth(), img.getWidth() % 11);
-				y += img.getHeight() % 11;
+			for (int y = 0; y < img.getHeight(); y++) {
+				if (y % (img.getHeight() / 11) == 0 && y > img.getHeight() % 11
+						&& y < img.getHeight() - img.getHeight() % 11) {
+					white = !white;
+				}
+				if (white) {
+					img.setRGB(x, y, (int) (img.getRGB(x, y) * 0.5 ));
+				}
+				else img.setRGB(x, y, (int) (img.getRGB(x, y) * 1.5));
 			}
 		}
 
@@ -35,3 +36,25 @@ public class Chess extends Edit {
 	}
 
 }
+
+// Fungerar för att göra ett rutmönster men vet ej om det är uppgiften.
+
+
+// Graphics g = img.getGraphics();
+// g.setColor(new Color(0, 0, 0));
+//
+// for (int x = 0; x < img.getWidth(); x++) {
+// if (x % (img.getWidth() / 11) == 0 && x > img.getWidth()%11 && x <
+// img.getWidth() - img.getWidth()%11) {
+// g.fillRect(x, 0, img.getWidth() % 11, img.getHeight());
+// x += img.getWidth() % 11;
+// }
+// }
+//
+// for (int y = 0; y < img.getHeight(); y++) {
+// if (y % (img.getHeight() / 11) == 0 && y > img.getHeight()%11 && y <
+// img.getHeight() - img.getHeight()%11) {
+// g.fillRect(0, y, img.getWidth(), img.getWidth() % 11);
+// y += img.getHeight() % 11;
+// }
+// }
--- app/Circle.java
+++ app/Circle.java
@@ -6,23 +6,14 @@ import java.awt.image.BufferedImage;
 
 import framework.Edit;
 
-
-/* TODO
- * Circle kommer inte i mittan av Y-led
- */
 public class Circle extends Edit {
 
 	public BufferedImage edit(BufferedImage img) {
 
 		Graphics g = img.getGraphics();
 		g.setColor(new Color(0, 0, 0));
-		g.fillArc(0, /*DENNA STÄMMER EJ*/ img.getHeight() - img.getWidth()/2, img.getWidth(), img.getWidth(), 0, 360);	
-		
-		
-		g.setColor(new Color(255, 255, 255));
-		g.drawString("KÅGE FIXA DENNA!", img.getWidth()/2 , img.getHeight()/2);
-		
-		
+		g.fillArc(0, img.getHeight()/2 - img.getWidth()/2, img.getWidth(), img.getWidth(), 0, 360);	
+	
 		return img;
 	}
 
