35d4d82c1241b95d6c8c42c8b6fee74de2d16ca1
C-Bom <Simon.cederbom@gmail.com>
2018-05-11 01:38:25 +0200

    småfixar
    Brightness funkar nu med grayscale
    
    Alla svarta linjer är borttagna från Swirl (fortfarande brus kvar) - borde använda BILINEAR för att få fram vaje pixels färg.

--- app/Brightness.java
+++ app/Brightness.java
@@ -1,6 +1,9 @@
 package app;
 
+import java.awt.color.ColorSpace;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorConvertOp;
+
 import java.awt.image.RescaleOp;
 
 import framework.ScaleableEdit;
@@ -9,10 +12,24 @@ public class Brightness extends ScaleableEdit {
 
 	public BufferedImage edit(BufferedImage img, int scale) {
 
-		float floatScale = (scale + (float)39.99899) / ((float) 100);
+		if (img.getColorModel().getColorSpace().getType() == ColorSpace.CS_GRAY) {
+			ColorSpace cs = ColorSpace.getInstance(ColorSpace.CS_sRGB);
+			ColorConvertOp op = new ColorConvertOp(cs, null);
+			op.filter(img, null);
 
-		RescaleOp rescaleOp = new RescaleOp(floatScale, 15, null);
-		rescaleOp.filter(img, img);
+			float floatScale = (scale + (float) 39.99899) / ((float) 100);
+			RescaleOp rescaleOp = new RescaleOp(floatScale, 15, null);
+			rescaleOp.filter(img, img);
+
+			cs = ColorSpace.getInstance(ColorSpace.CS_GRAY);
+			op = new ColorConvertOp(cs, null);
+			op.filter(img, null);
+			
+		} else {
+			float floatScale = (scale + (float) 39.99899) / ((float) 100);
+			RescaleOp rescaleOp = new RescaleOp(floatScale, 15, null);
+			rescaleOp.filter(img, img);
+		}
 
 		return img;
 	}
--- app/Swirl.java
+++ app/Swirl.java
@@ -1,16 +1,25 @@
 package app;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
+
 import framework.ScaleableEdit;
 
 public class Swirl extends ScaleableEdit {
 
+	
+	/*TODO
+	 *  Use bilinear interpolation to get the color to get rid of the noise.
+	 */
+	
 	public BufferedImage edit(BufferedImage img, int scale) {
 
 		double scaleDouble = (scale - 50) / ((double) 100);
 		final double width = img.getWidth();
 		final double height = img.getHeight();
-		BufferedImage sImg = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
+		BufferedImage sImg = deepCopy(img);
 		for (int x = 0; x < width; x++) {
 			double xr = x / width - 0.5;
 			for (int y = 0; y < height; y++) {
@@ -23,13 +32,34 @@ public class Swirl extends ScaleableEdit {
 				sImg.setRGB(xs, ys, img.getRGB(x, y));
 			}
 		}
-		for (int x = 5; x < width; x++) {
-			for (int y = 5; y < height; y++) {
-				if (sImg.getRGB(x, y) == 0) {
-					sImg.setRGB(x, y, sImg.getRGB(x - 5, y-5));
+		
+		/*removes some of the noise*/
+		for (int x = 1; x < width; x++) {
+			for (int y = 1; y < height - 1; y++) {
+				if (sImg.getRGB(x, y) == new Color(0, 0, 0).getRGB()) {
+					sImg.setRGB(x, y, sImg.getRGB(x - 1, y));
 				}
 			}
 		}
+		for (int x = 0; x < width -2; x++) {
+			for (int y = 0; y < height - 2; y++) {
+				if (sImg.getRGB(x, y) == img.getRGB(x,y)) {
+					sImg.setRGB(x, y, sImg.getRGB(x, y+1));
+				}
+				if (sImg.getRGB(x, y) == img.getRGB(x,y)) {
+					sImg.setRGB(x, y, sImg.getRGB(x+1, y));
+				}
+			}
+		}
+
 		return sImg;
 	}
+	
+	static BufferedImage deepCopy(BufferedImage bi) {
+		ColorModel cm = bi.getColorModel();
+		boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
+		WritableRaster raster = bi.copyData(null);
+		return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
+	}
+
 }
